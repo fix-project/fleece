5cc1fd0acb950717baf8da946f1a27b8e95c7ff6
Angela Montemayor <amontema@cs.stanford.edu>
2022-10-13 18:54:35 -0700

    first network call

--- src/frontend/yay.cc
+++ src/frontend/yay.cc
@@ -1,7 +1,22 @@
 #include <iostream>
+
+#include "socket.hh"
+
 using namespace std;
 
 int main()
 {
+  TCPSocket sock;
+  sock.connect({"www.cs.stanford.edu", "http"});
+  sock.write("GET / HTTP/1.1\r\nhost: www.cs.stanford.edu\r\n\r\n");
+
+  string buf;
+  buf.resize(1024);
+  string_span buf_span{string_span::from_view(buf)};
+
+  while (not sock.eof()) {
+    auto len = sock.read(buf_span);
+    cout << buf_span.substr(0, len);
+  }
   cout << "Hello World!\n";
 }
