5b4ae1c4758e9e868ee236018211b5ac46e23c0a
Keith Winstein <keithw@cs.stanford.edu>
2022-11-18 15:18:38 -0800

    Minimize sha256.hh/cc

--- src/util/sha256.cc
+++ src/util/sha256.cc
@@ -1,24 +1,12 @@
 #include <openssl/sha.h>
 
 #include "sha256.hh"
-#include "timer.hh"
 
 using namespace std;
 
-std::string sha256buf_to_string_( const unsigned char* buf )
+static_assert( tuple_size<sha256_hash>::value == SHA256_DIGEST_LENGTH );
+
+void sha256( const string_view input, sha256_hash& hash )
 {
-  char sbuf[2 * SHA256_DIGEST_LENGTH + 1];
-  for ( unsigned i = 0; i < SHA256_DIGEST_LENGTH; i++ ) {
-    snprintf( &( sbuf[2 * i] ), 3, "%2.2x", buf[i] );
-  }
-  return string( sbuf, 2 * SHA256_DIGEST_LENGTH );
-}
-
-std::string sha256::encode( string input )
-{
-  unsigned char output[SHA256_DIGEST_LENGTH];
-
-  SHA256( (const unsigned char*)input.c_str(), input.length(), output );
-
-  return sha256buf_to_string_( output );
+  SHA256( reinterpret_cast<const uint8_t*>( input.data() ), input.length(), hash.data() );
 }
--- src/util/sha256.hh
+++ src/util/sha256.hh
@@ -1,6 +1,7 @@
-#include <string>
+#include <array>
+#include <string_view>
 
-namespace sha256 {
-std::string encode( std::string input );
-std::string sha256buf_to_string_( const unsigned char* buf );
-}
+using sha256_hash = std::array<uint8_t, 32>; // equal to SHA256_DIGEST_LENGTH, but don't want to bring
+                                             // OpenSSL into the global namespace
+
+void sha256( const std::string_view input, sha256_hash& hash );
