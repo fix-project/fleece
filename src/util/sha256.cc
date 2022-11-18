cf1e9defd6c8ce04591ac2d5ab0ee4e113bbc1d2
Angela Montemayor <amontema@cs.stanford.edu>
2022-11-18 14:55:40 -0800

    sha256 from mush

---
+++ src/util/sha256.cc
@@ -0,0 +1,24 @@
+#include <openssl/sha.h>
+
+#include "sha256.hh"
+#include "timer.hh"
+
+using namespace std;
+
+string sha256buf_to_string_( const unsigned char* buf )
+{
+  char sbuf[2 * SHA256_DIGEST_LENGTH + 1];
+  for ( unsigned i = 0; i < SHA256_DIGEST_LENGTH; i++ ) {
+    snprintf( &( sbuf[2 * i] ), 3, "%2.2x", buf[i] );
+  }
+  return string( sbuf, 2 * SHA256_DIGEST_LENGTH );
+}
+
+string sha256::encode( string input )
+{
+  unsigned char output[SHA256_DIGEST_LENGTH];
+
+  SHA256( (unsigned char*)input.c_str(), input.length(), output );
+
+  return sha256buf_to_string_( output );
+}
---
+++ src/util/sha256.hh
@@ -0,0 +1,6 @@
+#include <string>
+
+namespace sha256 {
+std::string encode( std::string input );
+string sha256buf_to_string_( const unsigned char* buf );
+}
